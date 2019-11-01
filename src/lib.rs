38869fad164bb04feaf4246ce43a636a0fce3756
Njagi Mwaniki <njagi@urbanslug.com>
2019-11-01 14:58:18 +0300

    Re-export the adder module in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,1 @@
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(4, add_two(2));
-    }
-
-}
-
-
-pub fn add_two(i: i32) -> i32 {
-    i+2
-}
+pub use adder;
